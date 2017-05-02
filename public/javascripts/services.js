891586ab2c66f408e0ec1e56ee1fe801b21c099b
Shauan Ferreira Leite <shauanf@gmail.com>
2017-05-02 18:52:51 -0400

    upload files

---
+++ public/javascripts/services.js
@@ -0,0 +1,66 @@
+// function showDiv1() {
+//     document.getElementById('service-one').className = 'visible'
+//     document.getElementById('service-two').className = 'hidden';
+//     document.getElementById('service-three').className = 'hidden';
+//     document.getElementById('service-four').className = 'hidden';
+//     document.getElementById('serv-a').classList.add("activez");
+//     document.getElementById('serv-b').classList.remove = "activez";
+//     document.getElementById('serv-c').classList.remove = "activez";
+//     document.getElementById('serv-d').classList.remove = "activez";
+//
+//
+// }
+//
+// function showDiv2() {
+//     document.getElementById('service-two').className = 'visible'
+//     document.getElementById('service-one').className = 'hidden';
+//     document.getElementById('service-three').className = 'hidden';
+//     document.getElementById('service-four').className = 'hidden';
+//     document.getElementById('serv-b').classList.add("activez");
+//     document.getElementById('serv-a').classList.remove("activez");
+//     document.getElementById('serv-c').classList.remove("activez");
+//     document.getElementById('serv-d').classList.remove("activez");
+// }
+//
+// function showDiv3() {
+//     document.getElementById('service-three').className = 'visible'
+//     document.getElementById('service-one').className = 'hidden';
+//     document.getElementById('service-two').className = 'hidden';
+//     document.getElementById('service-four').className = 'hidden';
+//     document.getElementById('serv-c').classList.add("activez");
+//     document.getElementById('serv-a').classList.remove("activez");
+//     document.getElementById('serv-b').classList.remove("activez");
+//     document.getElementById('serv-d').classList.remove("activez");
+//
+// }
+//
+// function showDiv4() {
+//     document.getElementById('service-four').className = 'visible'
+//     document.getElementById('service-one').className = 'hidden';
+//     document.getElementById('service-two').className = 'hidden';
+//     document.getElementById('service-three').className = 'hidden';
+//     document.getElementById('serv-d').classList.add("activez");
+//     document.getElementById('serv-a').classList.remove("activez");
+//     document.getElementById('serv-b').classList.remove("activez");
+//     document.getElementById('serv-c').classList.remove("activez");
+// }
+
+
+/* ===========================================================
+   FEATURES TAB
+============================================================== */
+// $('.features-tab .tab-title').on('click', function(e) {
+//     if (!$(this).hasClass('current')) {
+//         $('.tab-title').removeClass('out');
+//         $('.tab-title.current').addClass('out');
+//         $('.features-tab .tab-title').removeClass('current');
+//         $(this).addClass('current');
+//     }
+//     e.preventDefault();
+// });
+
+
+
+/* ===========================================================
+   Effects
+============================================================== */
