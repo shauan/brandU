1507095ba1bb6cc438e30e761ce8bb02518b7033
Shauan Ferreira Leite <shauanf@gmail.com>
2017-05-08 17:54:26 -0400

    add pg contact with form :muscle:

--- public/javascripts/services.js
+++ public/javascripts/services.js
@@ -2,7 +2,7 @@
 jQuery(document).ready(function() {
 var aa = jQuery("#navscroll");
 jQuery(window).scroll(function() {
-if( jQuery(this).scrollTop() >  420 ) {
+if( jQuery(this).scrollTop() >  220 ) {
 aa.addClass("sticky");
 } else {
 aa.removeClass("sticky");
