539bcfa9bc80622f98e7ed1e1a1b7747b5a68019
Shauan Ferreira Leite <shauanf@gmail.com>
2017-05-08 16:18:44 -0400

    add style to navbar and jquery function when scroll :fire:

--- public/javascripts/services.js
+++ public/javascripts/services.js
@@ -1,66 +1,23 @@
-// function showDiv1() {
-//     document.getElementById('service-one').className = 'visible'
-//     document.getElementById('service-two').className = 'hidden';
-//     document.getElementById('service-three').className = 'hidden';
-//     document.getElementById('service-four').className = 'hidden';
-//     document.getElementById('serv-a').classList.add("activez");
-//     document.getElementById('serv-b').classList.remove = "activez";
-//     document.getElementById('serv-c').classList.remove = "activez";
-//     document.getElementById('serv-d').classList.remove = "activez";
 //
-//
-// }
-//
-// function showDiv2() {
-//     document.getElementById('service-two').className = 'visible'
-//     document.getElementById('service-one').className = 'hidden';
-//     document.getElementById('service-three').className = 'hidden';
-//     document.getElementById('service-four').className = 'hidden';
-//     document.getElementById('serv-b').classList.add("activez");
-//     document.getElementById('serv-a').classList.remove("activez");
-//     document.getElementById('serv-c').classList.remove("activez");
-//     document.getElementById('serv-d').classList.remove("activez");
-// }
-//
-// function showDiv3() {
-//     document.getElementById('service-three').className = 'visible'
-//     document.getElementById('service-one').className = 'hidden';
-//     document.getElementById('service-two').className = 'hidden';
-//     document.getElementById('service-four').className = 'hidden';
-//     document.getElementById('serv-c').classList.add("activez");
-//     document.getElementById('serv-a').classList.remove("activez");
-//     document.getElementById('serv-b').classList.remove("activez");
-//     document.getElementById('serv-d').classList.remove("activez");
-//
-// }
-//
-// function showDiv4() {
-//     document.getElementById('service-four').className = 'visible'
-//     document.getElementById('service-one').className = 'hidden';
-//     document.getElementById('service-two').className = 'hidden';
-//     document.getElementById('service-three').className = 'hidden';
-//     document.getElementById('serv-d').classList.add("activez");
-//     document.getElementById('serv-a').classList.remove("activez");
-//     document.getElementById('serv-b').classList.remove("activez");
-//     document.getElementById('serv-c').classList.remove("activez");
-// }
-
-
-/* ===========================================================
-   FEATURES TAB
-============================================================== */
-// $('.features-tab .tab-title').on('click', function(e) {
-//     if (!$(this).hasClass('current')) {
-//         $('.tab-title').removeClass('out');
-//         $('.tab-title.current').addClass('out');
-//         $('.features-tab .tab-title').removeClass('current');
-//         $(this).addClass('current');
-//     }
-//     e.preventDefault();
-// });
-
-
-
-/* ===========================================================
-   Effects
-============================================================== */
+// (function($) {
+//     $(document).ready(function(){
+//         $(window).scroll(function(){
+//             if ($(this).scrollTop() > 200) {
+//                 $('.navscroll').fadeIn(500);
+//             } else {
+//                 $('.navscroll').fadeOut(500);
+//             }
+//         });
+//     });
+// })(jQuery);
+jQuery(document).ready(function() {
+var aa = jQuery("#navscroll");
+jQuery(window).scroll(function() {
+//adjust the number to fit distance from the top of page to the section
+if( jQuery(this).scrollTop() >  320 ) {
+aa.addClass("sticky");
+} else {
+aa.removeClass("sticky");
+}
+});
+});
