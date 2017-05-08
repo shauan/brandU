e93c03c0c3ed3284077ad341cdcb81f70a4a0eea
Shauan Ferreira Leite <shauanf@gmail.com>
2017-05-08 16:25:14 -0400

    add links footer :squirrel:

--- public/javascripts/services.js
+++ public/javascripts/services.js
@@ -1,20 +1,8 @@
-//
-// (function($) {
-//     $(document).ready(function(){
-//         $(window).scroll(function(){
-//             if ($(this).scrollTop() > 200) {
-//                 $('.navscroll').fadeIn(500);
-//             } else {
-//                 $('.navscroll').fadeOut(500);
-//             }
-//         });
-//     });
-// })(jQuery);
+
 jQuery(document).ready(function() {
 var aa = jQuery("#navscroll");
 jQuery(window).scroll(function() {
-//adjust the number to fit distance from the top of page to the section
-if( jQuery(this).scrollTop() >  320 ) {
+if( jQuery(this).scrollTop() >  420 ) {
 aa.addClass("sticky");
 } else {
 aa.removeClass("sticky");
